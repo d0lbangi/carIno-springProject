6599a7becbcf4e20df572abc66196f83d9500c5d
Jeongho Park <d0lbangi13@naver.com>
2023-10-25 17:44:25 +0900

    initial environment setting

---
+++ src/test/java/com/d0lbangi/carinfo/CarinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d0lbangi.carinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
